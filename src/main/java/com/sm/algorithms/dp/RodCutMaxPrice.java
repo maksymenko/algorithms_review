cadfdc02065417dc24c8ab3077af1229f7228ad1
sergii <sergey.maksymenko@gmail.com>
2017-12-20 20:17:29 -0800

    rod cut solution reconstruction

--- src/main/java/com/sm/algorithms/dp/RodCutMaxPrice.java
+++ src/main/java/com/sm/algorithms/dp/RodCutMaxPrice.java
@@ -24,13 +24,34 @@ public class RodCutMaxPrice {
     return maxP;
   }
 
+  public int findSolutionByTopDown(int len) {
+    if (len < 1) {
+      return 0;
+    }
+
+    if (dp.get(len) != null) {
+      return dp.get(len);
+    }
+
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < len; i++) {
+      int newMax = prices[i] + findSolutionByTopDown(len - i - 1);
+      if (newMax > max) {
+        max = newMax;
+        dpSolution.put(len, i + 1);
+      }
+    }
+    System.out.println(">> findSolutionByTopDown for " + len + " max: " + max);
+    dp.put(len, max);
+    return max;
+  }
 
   public int findBottomUp(int len) {
     dp.put(0, 0);
 
-    for(int i = 1; i <= len; i++) {
+    for (int i = 1; i <= len; i++) {
       int max = Integer.MIN_VALUE;
-      for(int j = 0; j < i; j++) {
+      for (int j = 0; j < i; j++) {
         max = Math.max(max, prices[j] + dp.get(i - j - 1));
       }
       dp.put(i, max);
@@ -47,36 +68,48 @@ public class RodCutMaxPrice {
         int newMax = prices[j] + dp.get(i - j - 1);
         if (newMax > max) {
           max = newMax;
-          dpSolution.put(i, j+1);
+          dpSolution.put(i, j + 1);
         }
       }
       dp.put(i, max);
       System.out.println(">> findSolutionBottomUp for " + i + " max: " + max);
     }
 
-    // Show solution.
-    System.out.print(">> Solution:");
+    return dp.get(len);
+  }
+
+  private void reconstructSolution(int len) {
     int cursor = len;
+    System.out.print(">> Solution ");
     while(cursor > 0) {
       int step = dpSolution.get(cursor);
       System.out.print(" " + step);
       cursor -= step;
     }
     System.out.println();
-
-    return dp.get(len);
   }
 
-
+  private void init() {
+    dp.clear();
+    dpSolution.clear();
+  }
 
   public static void main(String[] args) {
     RodCutMaxPrice rodCutMaxPrice = new RodCutMaxPrice();
     System.out.println(">>> starting...");
+
+    rodCutMaxPrice.init();
     System.out.println(">> max by TopDown for 4 is: " + rodCutMaxPrice.findTopDown(4));
+
+    rodCutMaxPrice.init();
     System.out.println(">> max by BottomUp for 4 is: " + rodCutMaxPrice.findBottomUp(4));
-    System.out.println(">> max by BottomUp for 4 is: " + rodCutMaxPrice.findBottomUp2(4));
-    System.out.println(">> max by FindSolutionBottomUp for 4 is: " + rodCutMaxPrice.findSolutionBottomUp(9));
 
+    rodCutMaxPrice.init();
+    System.out.println(">> max by FindSolutionBottomUp for 9 is: " + rodCutMaxPrice.findSolutionBottomUp(9));
+    rodCutMaxPrice.reconstructSolution(9);
+
+    rodCutMaxPrice.init();
+    System.out.println(">> max by findSolutionByTopDown for 9 is: " + rodCutMaxPrice.findSolutionByTopDown(9));
+    rodCutMaxPrice.reconstructSolution(9);
   }
-
 }
